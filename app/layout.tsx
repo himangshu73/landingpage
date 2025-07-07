e66611112c49c44a774aa50d8f6040efd65f0cfd
himangshu <himangshu73@gmail.com>
2025-07-07 23:49:11 +0600

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Urbanist } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const urbanist = Urbanist({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: "--font-urbanist",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -23,12 +19,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="en" className={`${urbanist.variable}`}>
+      <body className="font-sans antialiased">{children}</body>
     </html>
   );
 }
---
+++ components/navbar.tsx
@@ -0,0 +1,24 @@
+export default function Navbar() {
+  return (
+    <nav className="flex items-center justify-between py-8">
+      <div className="font-semibold text-[22px] sm:text-[26px] md:text-[30px] lg:text-[34px] xl:text-4xl">
+        Desgy Solutions
+      </div>
+      <div className="flex items-center gap-2">
+        <div>Latest Works</div>
+        <div>View All</div>
+      </div>
+      <div className="flex gap-2">
+        <div>About Us</div>
+        <div>Services</div>
+        <div>FAQ</div>
+        <div>Blog</div>
+      </div>
+      <div>
+        <button className="border border-1 rounded-full px-4 py-2 font-semibold">
+          Contact Us
+        </button>
+      </div>
+    </nav>
+  );
+}
